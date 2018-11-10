3bb2a40321e914aeb7157d3df69367654ee9fcae
Michael Filonenko <michaelfilonenko@MacBook-Pro.local>
2018-11-10 15:18:19 +0200

    first backend request

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import './scss/main.scss';
-alert("test");
+const SERVER_NAME = "http://nit.tron.net.ua/";
+function createXHR() {
+    try {
+        return new XMLHttpRequest();
+    } catch(e) {
+        try {
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        } catch(e) {
+            return new ActiveXObject("Msxml2.XMLHTTP");
+        }
+    }
+}
+function loadCategories(server, method) {
+    const xhr = createXHR();
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            document.getElementById("test").innerHTML = this.responseText;
+        }
+    }
+    xhr.open("GET", server + method, false);
+    xhr.send();
+}
+window.onload = function() {
+    loadCategories(SERVER_NAME, "api/category/list");
+}
