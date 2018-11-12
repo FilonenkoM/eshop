e1dcbd2f7d52d8df18747c09a6e00724ac0d3ca1
Michael Filonenko <michaelfilonenko@MacBook-Pro.local>
2018-11-12 19:55:23 +0200

    html templates included.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './scss/main.scss';
 
 const SERVER_NAME = "http://nit.tron.net.ua/";
+var CATEGORY_TEMPLATE;
+var PRODUCT_TEMPLATE;
 
 function createXHR() {
     try {
@@ -14,73 +16,70 @@ function createXHR() {
     }
 }
 
-function load(server, method, onLoad) {
+function load(server, method, onLoad, onLoadArguments) {
     const xhr = createXHR();
     xhr.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            onLoad(JSON.parse(this.responseText));
+            onLoadArguments.push(JSON.parse(this.responseText));
+            onLoad.apply(null, onLoadArguments);
         }
-    }
+    };
     xhr.open("GET", server + method, true);
     xhr.send();
 }
 
-function createCategoryTemplate() {
-    var template = document.createElement("li");
-    return template;
-}
-function message() {
-    alert("hello");
-}
-var categoryTemplate = createCategoryTemplate();
-
-function createProductTemplate() {
-    var template = document.createElement("li");
-    return template;
-}
-var productTemplate = createProductTemplate();
-
-function insertCategories(categories) {
-    for (let i = 0; i < categories.length; i++) {
-        const view = categoryTemplate.cloneNode();
-        view.onclick = function() {
-            const id = categories[i].id;
-            load(SERVER_NAME, "api/product/list/category/" + id, insertProducts);
-        }
-        view.innerHTML = "<a href='#'>" + categories[i].name + "</a>";
-        document.getElementById("categories").appendChild(view);
-    }
+function fillProduct(product) {
+    var view = PRODUCT_TEMPLATE.content.cloneNode(true);
+    var link = view.getElementById("product-name-link");
+    link.onclick = function () {
+        showProductView(product);
+    };
+    link.innerHTML = product.name;
+    return view;
 }
 
-function insertProducts(products) {
-    let container = document.getElementById("products");
+function fillCategory(category) {
+    var view = CATEGORY_TEMPLATE.content.cloneNode(true);
+    var link = view.getElementById("category-name-link");
+    link.onclick = function () {
+        load(SERVER_NAME, "api/product/list/category/" + category.id, insertTemplatedContent,
+            [document.getElementById("products"), fillProduct]);
+    };
+    link.innerHTML = category.name;
+    return view;
+}
+
+function insertTemplatedContent(container, fill, array) {
     container.innerHTML = "";
-    for (let i = 0; i < products.length; i++) {
-        const view = productTemplate.cloneNode();
-        view.onclick = function() {
-            showProductView(products[i]);
-        }
-        view.innerHTML = "<a href='#'>" +  products[i].name + "</a>";
-        container.appendChild(view);
+    for (let i = 0; i < array.length; i++) {
+        container.appendChild(fill(array[i]));
     }
 }
+
 function showProductView(product) {
     document.getElementById("list-view").style.display = "none";
     const view = document.getElementById("product-view");
     view.style.display = "block";
     document.getElementById("product-view-name").innerHTML = product.name;
 }
+
 function showListView() {
     document.getElementById("product-view").style.display = "none";
     document.getElementById("list-view").style.display = "block";
 }
+
 function setupProductView() {
     document.getElementById("back-button").onclick = showListView;
 }
+
 window.onload = function () {
-    load(SERVER_NAME, "api/category/list", insertCategories);
-    load(SERVER_NAME, "api/product/list", insertProducts);
+    CATEGORY_TEMPLATE = document.getElementById("category-template");
+    PRODUCT_TEMPLATE = document.getElementById("product-template");
+    load(SERVER_NAME, "api/category/list", insertTemplatedContent,
+        [document.getElementById("categories"), fillCategory]);
+    load(SERVER_NAME, "api/product/list", insertTemplatedContent,
+        [document.getElementById("products"), fillProduct]);
     setupProductView();
-}
+};
 
 
