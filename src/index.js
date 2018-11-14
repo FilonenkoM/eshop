df2895ff8b1f35eaab4a4b2031b7214612636806
Michael Filonenko <michaelfilonenko@MacBook-Pro.local>
2018-11-14 14:50:13 +0200

    attempt to add post request

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import './scss/main.scss';
 
 const SERVER_NAME = "http://nit.tron.net.ua/";
 const CATEGORY_TEMPLATE = document.getElementById("category-template");
-const PRODUCT_TEMPLATE = document.getElementById("product-template");
+const PRODUCT_TEMPLATE = document.getElementById("product-template")
+
+var cartTable = document.getElementById("cart-view-table");
 var itemsInCart = [];
 var countOfItemsInCart = [];
 
@@ -40,12 +42,22 @@ function fillProduct(product) {
     view.getElementById("product-image").src = product.image_url;
     view.getElementById("product-price").innerHTML = product.price;
     view.getElementById("product-special-price").innerHTML = product.special_price;
-    view.getElementById("add-to-cart").onclick = function() {
-        itemsInCart.push(product);
-        countOfItemsInCart.push(1);
-
+    view.getElementById("add-to-cart").onclick = function () {
+        addRowToCartTable(cartTable, product);
     }
     return view;
+}
+
+function addRowToCartTable(table, product) {
+    if (!itemsInCart.includes(product.id)) {
+        itemsInCart.push(product.id);
+        countOfItemsInCart.push(1);
+        var row = cartTable.insertRow(cartTable.rows.length);
+        row.insertCell(0).innerHTML = product.name;
+        row.insertCell(1).innerHTML = product.price;
+        row.insertCell(2).innerHTML = "1";
+        row.insertCell(3).innerHTML = "Видалити";
+    }
 }
 
 function fillCategory(category) {
@@ -58,11 +70,21 @@ function fillCategory(category) {
     link.innerHTML = category.name;
     return view;
 }
+
 function insertTemplatedContent(container, fill, array) {
     container.innerHTML = "";
     for (let i = 0; i < array.length; i++) {
         container.appendChild(fill(array[i]));
     }
+}
+
+function post(server, method, params) {
+    var xhr = createXHR();
+    xhr.open("POST", serve + method, true);
+
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
 }
 
 function showProductView(product) {
@@ -74,15 +96,17 @@ function showProductView(product) {
     document.getElementById("product-view-price").innerText = product.price;
     document.getElementById("product-view-special-price").innerHTML = product.special_price;
     document.getElementById("product-view-description").innerHTML = product.description;
+    document.getElementById("to-cart-button").onclick = function () {
+        var cartTable = document.getElementById("cart-view-table")
+        addRowToCartTable(cartTable, product);
+        showCartView();
+    }
 }
+
 function showCartView() {
     document.getElementById("product-view").style.display = "none";
     document.getElementById("list-view").style.display = "none";
     document.getElementById("cart-view").style.display = "block";
-    fillCartTable();
-}
-function fillCartTable() {
-    const table = document.getElementById("cart-view-table")
 }
 
 function showListView() {
@@ -92,16 +116,33 @@ function showListView() {
 
 function setupProductView() {
     document.getElementById("back-button").onclick = showListView;
-    document.getElementById("to-cart-button").onclick = showCartView;
+}
+
+function setupCartView() {
+    document.getElementById("cart-view-send").onclick = function () {
+        var email = document.getElementById("cart-view-email").value;
+        var name = document.getElementById("cart-view-name").value;
+        var phone = document.getElementById("cart-view-phone").value;
+        var params = {
+            "name": name,
+            "phone": phone,
+            "email": email,
+        };
+        for(let i=0;i<itemsInCart;i++) {
+            var param = "products[" + itemsInCart[i] + "]";
+            params[param] = countOfItemsInCart[i];
+        }
+        post(SERVER_NAME, "api/order/add", params);
+    }
 }
 
 window.onload = function () {
-
     load(SERVER_NAME, "api/category/list", insertTemplatedContent,
         [document.getElementById("categories"), fillCategory]);
     load(SERVER_NAME, "api/product/list", insertTemplatedContent,
         [document.getElementById("products"), fillProduct]);
     setupProductView();
+    setupCartView();
 };
 
 
